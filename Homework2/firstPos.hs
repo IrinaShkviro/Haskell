453b64048df60c7eee855907f018b233e533d142
IrinaShkviro <IrinaShkviro13@gmail.com>
2014-02-27 17:18:05 +0400

    Some corrections

--- Homework2/firstPos.hs
+++ Homework2/firstPos.hs
@@ -1,14 +1,8 @@
 firstPos1 :: Int -> [Int] -> Int -> Int
-
 firstPos1 _ [] _ = 0
-
 firstPos1 n (x : xs) c = if n == x 
 	then c
-
 	else firstPos1 n xs (c + 1)
 
-
-
 firstPos :: Int -> [Int] -> Int
-
 firstPos n s = firstPos1 n s 1
