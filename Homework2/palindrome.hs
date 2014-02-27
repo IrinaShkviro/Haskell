8959d6afdd34c5402833356b24a6ee89f79fce6b
IrinaShkviro <IrinaShkviro13@gmail.com>
2014-02-27 17:21:14 +0400

    Some corrections

--- Homework2/palindrome.hs
+++ Homework2/palindrome.hs
@@ -1,19 +1,11 @@
 withoutSpaces :: [Char] -> [Char]
-
 withoutSpaces [] = []
-
 withoutSpaces (s : xs) = if (s == ' ') 
 	then withoutSpaces xs
-
 	else s : withoutSpaces xs
 
-
-
 isPalindrome :: [Char] -> Bool
-
 isPalindrome s = if (reverse t == t) 
 	then True
-
 	else False
-
 	where t = withoutSpaces s 
