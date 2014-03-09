9b35fc0e3943535b30722800d6737804a4e1d7db
IrinaShkviro <IrinaShkviro13@gmail.com>
2014-03-09 19:58:45 +0400

    With three types of brackets

--- Homework3/correctness.hs
+++ Homework3/correctness.hs
@@ -1,12 +1,16 @@
-corr1 :: [Char] -> Int -> Bool
-corr1 [] c = (c == 0)
-corr1 (x : xs) c = if (x == '(') 
-	then corr1 xs (c + 1)
-	else if (x == ')')
-		then if (c < 1)
-			then False
-			else corr1 xs (c - 1)
-		else corr1 xs c
+corr1 :: [Char] -> Int -> Int -> Int -> Bool
+corr1 [] c1 c2 c3 = (c1 == 0) && (c2 == 0) && (c3 == 0)
+corr1 (x : xs) c1 c2 c3 = case x of
+    '(' -> corr1 xs (c1 + 1) c2 c3
+    ')' -> if (c1 < 1) then False
+        else corr1 xs (c1 - 1) c2 c3
+    '{' -> corr1 xs c1 (c2 + 1) c3
+    '}' -> if (c2 < 1) then False
+        else corr1 xs c1 (c2 - 1) c3
+    '[' -> corr1 xs c1 c2 (c3 + 1)
+    ']' -> if (c3 < 1) then False
+        else corr1 xs c1 c2 (c3 - 1)
+    _ -> corr1 xs c1 c2 c3
 
 corr :: [Char] -> Bool
-corr x = corr1 x 0
+corr x = corr1 x 0 0 0
