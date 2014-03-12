f294604ab0dbe9c31c03606123f2551b410cf9aa
IrinaShkviro <IrinaShkviro13@gmail.com>
2014-03-12 22:09:07 +0400

    Correct program correctness

--- Homework3/correctness.hs
+++ Homework3/correctness.hs
@@ -1,16 +1,17 @@
-corr1 :: [Char] -> Int -> Int -> Int -> Bool
-corr1 [] c1 c2 c3 = (c1 == 0) && (c2 == 0) && (c3 == 0)
-corr1 (x : xs) c1 c2 c3 = case x of
-    '(' -> corr1 xs (c1 + 1) c2 c3
-    ')' -> if (c1 < 1) then False
-        else corr1 xs (c1 - 1) c2 c3
-    '{' -> corr1 xs c1 (c2 + 1) c3
-    '}' -> if (c2 < 1) then False
-        else corr1 xs c1 (c2 - 1) c3
-    '[' -> corr1 xs c1 c2 (c3 + 1)
-    ']' -> if (c3 < 1) then False
-        else corr1 xs c1 c2 (c3 - 1)
-    _ -> corr1 xs c1 c2 c3
+corr :: [Char] -> [Char] -> Bool
+corr [] [] = True
+corr [] _ = False
+corr (x : xs) l = case x of
+	'(' -> corr xs (x : l)
+	'[' -> corr xs (x : l)
+	'{' -> corr xs (x : l)
+	')' -> if ((l /= []) && (head l) == '(') then corr xs (tail l)
+		else False
+	']' -> if ((l /= []) && (head l) == '[') then corr xs (tail l)
+		else False
+	'}' -> if ((l /= []) && (head l) == '{') then corr xs (tail l)
+		else False
+	_ -> corr xs l
 
-corr :: [Char] -> Bool
-corr x = corr1 x 0 0 0
+correct :: String -> Bool
+correct x = corr x []
