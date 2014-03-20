564b546eecc4f3cc4cc3306e538569e93a02d4cd
IrinaShkviro <IrinaShkviro13@gmail.com>
2014-03-20 22:27:42 +0400

    third task

---
+++ Homework5/comboes.hs
@@ -0,0 +1,13 @@
+import Control.Monad
+
+listing:: [Int] -> Int -> [[Int]]
+listing list n = if (n > 1) then return list ++ listing list (n - 1)
+ 		else return list
+
+allCombinations :: [[Int]] -> [[Int]]
+allCombinations [] = return []
+allCombinations (l:ls) = l >>= \v -> allCombinations ls >>= \vs -> return (v : vs)
+
+combo :: Int -> [[Int]]
+combo n = allCombinations (listing [1, 2, 3] n)
+			
